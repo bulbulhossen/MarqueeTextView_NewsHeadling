861538b157294de2917b2c59d8c9f69c65d9fe09
bulbulhossen <info247.bulbul@gmail.com>
2016-10-05 17:25:34 +0600

    Initial commit

---
+++ app/src/main/java/com/example/bulbulhossen/marqueetextview_newsheadling/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bulbulhossen.marqueetextview_newsheadling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.marqueetextview);
+        textView.setSelected(true);
+
+    }
+}
